443bf37f7c6cb7e43929bb804246c21a17396fdb
GOWRISHANKAR17801 <126137943+GOWRISHANKAR17801@users.noreply.github.com>
2023-04-16 08:46:00 +0530

    Add files via upload

---
+++ SQL-Project About the Alumni.sql
@@ -0,0 +1,221 @@
+-- ALUMNI STUDENTS
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+/* In this project SQL project about the almni students of diffrent colleges & which department they were studied,
+then which batch the students get passedout these things are found*/
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+# Create new schema as alumni
+CREATE DATABASE alumni;
+USE alumni;
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+
+# Importin the .csv files into MySQL workbench
+-- CSV files are IMPORTED
+select * from college_a_hs;
+select * from college_a_se;
+select * from college_a_sj;
+select * from college_b_hs;
+select * from college_b_se;
+select * from college_b_sj;
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+
+# Running the SQL command to see the structure of six tables
+
+DESC college_a_hs;
+DESC college_a_se;
+DESC college_a_sj;
+DESC college_b_hs;
+DESC college_b_se;
+DESC college_b_sj;
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+
+/* Displayed the first 1000 rows of tables in python workspace.
+(College_A_HS, College_A_SE, College_A_SJ, College_B_HS, College_B_SE, College_B_SJ) with Python.*/
+SELECT * FROM college_a_hs LIMIT 1000;
+SELECT * FROM college_a_se LIMIT 1000;
+SELECT * FROM college_a_sj LIMIT 1000;
+SELECT * FROM college_b_hs LIMIT 1000;
+SELECT * FROM college_b_se LIMIT 1000;
+SELECT * FROM college_b_sj LIMIT 1000;
+-- Using mysql.connector library in python I impoted the schema in my python workspace.
+-- Displayed the given statements in jupyter Notebook.alter
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+/* Imported first 1500 rows of tables 
+(College_A_HS, College_A_SE, College_A_SJ, College_B_HS, College_B_SE, College_B_SJ) into MS Excel */
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+/* Performing data cleaning on table College_A_HS and store cleaned data in view 
+College_A_HS_V, Remove null values */
+CREATE OR REPLACE VIEW college_a_hs_V as SELECT * FROM college_a_hs WHERE RollNo IS NOT NULL
+AND LastUpdate IS NOT NULL AND Name IS NOT NULL AND FatherName IS NOT NULL AND MotherName IS NOT NULL 
+AND BATCH IS NOT NULL AND Degree IS NOT NULL AND PresentStatus IS NOT NULL AND Location IS NOT NULL;
+SELECT * FROM college_a_hs_V;
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+
+/* Perform data cleaning on table College_A_SE and store 
+cleaned data in view College_A_SE_V to Remove null values.*/
+CREATE OR REPLACE VIEW college_a_se_V as SELECT * FROM college_a_se WHERE RollNo IS NOT NULL
+AND LastUpdate IS NOT NULL AND Name IS NOT NULL AND FatherName IS NOT NULL AND MotherName IS NOT NULL 
+AND BATCH IS NOT NULL AND Degree IS NOT NULL AND PresentStatus IS NOT NULL AND Organization IS NOT NULL 
+AND Location IS NOT NULL;
+SELECT * FROM college_a_se_V;
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+
+/* Performing the data cleaning on table College_A_SJ and 
+store cleaned data in view College_A_SJ_V to Remove null values.*/
+CREATE OR REPLACE VIEW college_a_sj_V as SELECT * FROM college_a_sj WHERE RollNo IS NOT NULL
+AND LastUpdate IS NOT NULL AND Name IS NOT NULL AND FatherName IS NOT NULL AND MotherName IS NOT NULL 
+AND BATCH IS NOT NULL AND Degree IS NOT NULL AND PresentStatus IS NOT NULL AND Organization IS NOT NULL 
+AND Designation IS NOT NULL AND Location IS NOT NULL;
+SELECT * FROM college_a_sj_V;
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+
+/* Performing the data cleaning on table College_B_HS and 
+store cleaned data in view College_B_HS_V, Remove null values.*/
+
+CREATE OR REPLACE VIEW college_b_hs_V as SELECT * FROM college_b_hs WHERE RollNo IS NOT NULL
+AND LastUpdate IS NOT NULL AND Name IS NOT NULL AND FatherName IS NOT NULL AND MotherName IS NOT NULL 
+AND BRANCH IS NOT NULL AND BATCH IS NOT NULL AND Degree IS NOT NULL AND PresentStatus 
+IS NOT NULL AND HSDegree IS NOT NULL 
+AND EntranceExam IS NOT NULL AND Institute IS NOT NULL AND Location IS NOT NULL;
+SELECT * FROM college_b_hs_V;
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+
+/* Performing data cleaning on table College_B_SE and 
+store cleaned data in view College_B_SE_V to Remove null values.*/
+CREATE OR REPLACE VIEW college_b_se_V as SELECT * FROM college_b_se WHERE RollNo IS NOT NULL
+AND LastUpdate IS NOT NULL AND Name IS NOT NULL AND FatherName IS NOT NULL AND MotherName IS NOT NULL 
+AND BRANCH IS NOT NULL AND BATCH IS NOT NULL AND Degree IS NOT NULL AND PresentStatus 
+IS NOT NULL AND Organization IS NOT NULL AND Location IS NOT NULL;
+SELECT * FROM college_b_se_V;
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+
+/* Performing the data cleaning on table College_B_SJ and 
+store cleaned data in view College_B_SJ_V to Remove null values.*/
+CREATE OR REPLACE VIEW college_b_sj_V as SELECT * FROM college_b_sj WHERE RollNo IS NOT NULL
+AND LastUpdate IS NOT NULL AND Name IS NOT NULL AND FatherName IS NOT NULL AND MotherName IS NOT NULL 
+AND BRANCH IS NOT NULL AND BATCH IS NOT NULL AND Degree IS NOT NULL AND PresentStatus 
+IS NOT NULL AND Organization IS NOT NULL AND Designation IS NOT NULL AND Location IS NOT NULL;
+SELECT * FROM college_b_sj_V;
+DESC college_b_sj_V;
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+
+/* Making procedure to use string functions for converting record of 
+Name, FatherName, MotherName into lower case for views 
+(College_A_HS_V, College_A_SE_V, College_A_SJ_V, College_B_HS_V, College_B_SE_V, College_B_SJ_V)*/
+
+-- College_a_hs_v
+SELECT LOWER(Name),LOWER(FatherName),LOWER(MotherName) FROM college_a_hs_V;
+
+-- College_a_se_v
+SELECT LOWER(Name),LOWER(FatherName),LOWER(MotherName) FROM college_a_se_V;
+
+-- College_a_sj_v
+SELECT LOWER(Name),LOWER(FatherName),LOWER(MotherName) FROM college_a_sj_V;
+
+-- College_b_hs_V
+SELECT LOWER(Name),LOWER(FatherName),LOWER(MotherName) FROM college_b_hs_V;
+
+-- College_b_se_v
+SELECT LOWER(Name),LOWER(FatherName),LOWER(MotherName) FROM college_b_se_V;
+
+-- College_b_sj_v
+SELECT LOWER(Name),LOWER(FatherName),LOWER(MotherName) FROM college_a_sj_V;
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+
+/* Writing a query to create a procedure as get_name_collegeA using the cursor to 
+fetching names of all students from college A.*/
+
+DELIMITER $$
+CREATE PROCEDURE get_name_collegeA (INOUT naming TEXT(40000))
+BEGIN 
+DECLARE finished INT DEFAULT 0;
+DECLARE namelist VARCHAR(16000) DEFAULT"";
+DECLARE namedetail 
+CURSOR FOR 
+SELECT Name FROM college_a_hs_V 
+UNION SELECT Name FROM college_a_se_V 
+UNION SELECT Name FROM college_a_sj_V;
+DECLARE CONTINUE HANDLER FOR NOT FOUND SET finished = 1;
+OPEN namedetail;
+getname:LOOP 
+FETCH namedetail INTO namelist;
+IF finished = 1 THEN LEAVE getname;
+END IF;
+SET naming = CONCAT(namelist,";",naming);
+END LOOP getname;
+CLOSE namedetail;
+END $$
+DELIMITER ;
+
+SET @naming="";
+CALL get_name_collegeA(@naming);
+SELECT @naming Names_of_collegeA;
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+/* Writing a query to create procedure 
+get_name_collegeB using the cursor to fetching names of all students from college B.*/
+DELIMITER $$
+CREATE PROCEDURE get_name_collegeB (INOUT naming TEXT(40000))
+BEGIN 
+DECLARE finished INT DEFAULT 0;
+DECLARE namelist VARCHAR(16000) DEFAULT"";
+DECLARE namedetail 
+CURSOR FOR 
+SELECT Name FROM college_b_hs_V 
+UNION SELECT Name FROM college_b_se_V 
+UNION SELECT Name FROM college_b_sj_V;
+DECLARE CONTINUE HANDLER FOR NOT FOUND SET finished = 1;
+OPEN namedetail;
+getname:LOOP 
+FETCH namedetail INTO namelist;
+IF finished = 1 THEN LEAVE getname;
+END IF;
+SET naming = CONCAT(namelist,";",naming);
+END LOOP getname;
+CLOSE namedetail;
+END $$
+DELIMITER ; 
+
+SET @naming="";
+CALL get_name_collegeB(@naming);
+SELECT @naming Names_of_collegeB;
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+/* .Calculating the percentage of career choice of College A and College B Alumni*/
+
+SELECT "HigherStudies" PresentStatus,(SELECT COUNT(*) FROM college_a_hs)/
+((SELECT COUNT(*) FROM college_a_hs)+(SELECT COUNT(*) FROM college_a_se)+
+(SELECT COUNT(*) FROM college_a_sj))*100 College_A_Percentage,
+(SELECT COUNT(*) FROM college_b_hs)/
+((SELECT COUNT(*) FROM college_b_hs)+(SELECT COUNT(*) FROM college_b_se)+
+(SELECT COUNT(*) FROM college_b_sj))*100 College_B_Percentage UNION 
+SELECT "Self Employed"PresentStatus,(SELECT COUNT(*) FROM college_a_se)/
+((SELECT COUNT(*) FROM college_a_hs)+(SELECT COUNT(*) FROM college_a_se)+
+(SELECT COUNT(*) FROM college_a_sj))*100 College_A_Percentage,
+(SELECT COUNT(*) FROM college_b_se)/
+((SELECT COUNT(*) FROM college_b_hs)+(SELECT COUNT(*) FROM college_b_se)+
+(SELECT COUNT(*) FROM college_b_sj))*100 College_B_Percentage UNION
+SELECT "Service Job"PresentStatus,(SELECT COUNT(*) FROM college_a_sj)/
+((SELECT COUNT(*) FROM college_a_hs)+(SELECT COUNT(*) FROM college_a_se)+
+(SELECT COUNT(*) FROM college_a_sj))*100 College_A_Percentage,
+(SELECT COUNT(*) FROM college_b_se)/
+((SELECT COUNT(*) FROM college_b_hs)+(SELECT COUNT(*) FROM college_b_sj)+
+(SELECT COUNT(*) FROM college_b_sj))*100 College_B_Percentage;
+
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
